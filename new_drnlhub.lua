7e05e6f4044b355a90b1bc7b94f4119d21360b21
ant-sketch655 <thalysstahl3@gmail.com>
2026-08-29 15:34:27 -0300

    Update new_drnlhub.lua

--- new_drnlhub.lua
+++ new_drnlhub.lua
@@ -57,7 +57,7 @@ Instance.new("UICorner", title).CornerRadius = UDim.new(0, 10)
 local comandos = {
     {nome = "//armar c4", cmd = "//Armar C4", vezes = 1},
     {nome = "//ativar c4", cmd = "//Ativar C4", vezes = 1},
-    {nome = "//mat", cmd = "/TIRO NA CABEÇA -[FAMÍLIA DORNELLE] -🇮🇹", vezes = 3},
+    {nome = "//mat", cmd = "/TIRO NA CABEÇA", vezes = 3},
     {nome = "//Furar pneu", cmd = "//Furar pneu", vezes = 3},
     {nome = "//tiro no rosto", cmd = "//TIRO NO ROSTO -[FAMÍLIA DORNELLE]-🇮🇹", vezes = 3},
     {nome = "//Derrubar no chão", cmd = "//Derrubar no chão", vezes = 1},
